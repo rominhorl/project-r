10c12e464e8bf889f59d85a69c1c5078829a699f
rominhorl <romulorochalima@gmail.com>
2022-08-21 16:05:24 -0300

    Finished the base os dialog system

---
+++ project-r/DialogDatabase.gd
@@ -0,0 +1,14 @@
+extends Node
+
+const dialogDatabase = {
+	"0" : {
+		"0" : { "name": "Dog", "text": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"},
+		"1" : { "name": "Cat", "text": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"},
+		"2" : { "name": "Piruleta", "text": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"},
+		"3" : { "name": "Dog", "text": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"},
+		"4" : { "name": "Cat", "text": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"},
+		}
+
+
+}
+
